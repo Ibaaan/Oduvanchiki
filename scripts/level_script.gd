a8fcd5acea9ecd212452b511c638b5cd7cef141e
Ibaaan <kuuuuushtanov@gmail.com>
2025-06-23 18:17:57 +0500

    Revert "Добавил две стадии игры: добавление цветка и"
    This reverts commit 666622775ddf7b8d924f7c187501da3c4f3defcf.

--- scripts/level_script.gd
+++ scripts/level_script.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-signal flower_pressed()	
+signal change_state_to_dir()	
 
 enum Tile{
 	WALL,
@@ -50,7 +50,7 @@ func get_tiles() -> Dictionary:
 func _on_flower_pressed(coords:Vector2) -> void:
 	grid[coords.y][coords.x] = Tile.FLOWER
 	#print(grid)
-	flower_pressed.emit()
+	change_state_to_dir.emit()
 
 func place_seeds(direction:Vector2):
 	
@@ -86,15 +86,3 @@ func print_grid():
 			res += str(grid[x][y]) + " "
 		print(res)
 	print()
-
-
-func is_fulled():
-	var len_grid := len(grid)
-	
-	for x in range(len_grid):
-		var len_grid_x := len(grid[x]) 
-		for y in range(len_grid_x):
-			if grid[y][x] == Tile.EMPTY:
-				print(grid[y][x])
-				return false
-	return true
